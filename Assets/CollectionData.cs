39ea87a475802672a6db73b350711a77a903a08d
小泉範明 <noriaki@koizuminoriakinoMacBook-Pro.local>
2022-01-08 23:56:33 +0900

    CSV読み込みのためのファイル群を追加

---
+++ Assets/CollectionData.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class CollectionData
+{
+    public int collection_id;
+    public int quiz_id;
+    public string title;
+    public string details;
+    public int open_flag;
+}
