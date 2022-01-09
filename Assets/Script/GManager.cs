cb7ea93922408459c973a07446d6f783bc449773
deratokati88 <74710163+deratokati88@users.noreply.github.com>
2022-01-09 15:11:22 +0900

    スタート画面を追加
    スタート画面からボタンを押すと名前登録画面へ移動する処理を追加しました。

---
+++ Assets/Script/GManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲーム全体をコントロールするスクリプト
+public class GManager : MonoBehaviour
+{
+    public static GManager instance = null;
+
+    [Header("名前")] public string player_name;
+
+
+    // シーンに一つしか作成されないように
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
